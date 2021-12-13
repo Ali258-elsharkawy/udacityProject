44ed564630dcd7f627d9486a04562c7b16bcc9ad
Ali258-elsharkawy <83187254+Ali258-elsharkawy@users.noreply.github.com>
2021-12-13 10:20:42 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,67 @@
+const navbar = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section")
+
+for(const section of sections){
+    const createLi= document.createElement("li")
+    const createLink =document.createElement("a")
+    createLink.href = "#"+section.id;
+    createLink.innerHTML= section.dataset.nav
+    createLink.setAttribute('id', 'link')
+    navbar.appendChild(createLi)
+    createLi.appendChild(createLink)
+}
+
+
+// }
+var isInViewport = function(elem){
+    var distance = elem.getBoundingClientRect()
+    return(
+        distance.top >=0 &&
+        distance.left >=0 &&
+        distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+}
+window.addEventListener('scroll',function(event){
+    sections.forEach(Element=> {
+        if (isInViewport(Element)){
+            Element.classList.add("your-active-class")
+        }
+        else{
+            Element.classList.remove("your-active-class")
+        }
+    });
+};
+
+    
+////// I tried tihs method befor asking in stackOverflow and it did not work, can i now how
+///// fix this method? /////////////////
+
+
+
+// function addSection(){
+//     const ul = document.getElementById("navbar__list");
+//     var section = document.querySelectorAll("section");
+//     for( let i= 0; i< section.length; i++){
+//         var li= ul.appendChild(document.createElement("li"));
+//         link=document.createElement("a");
+//         li.appendChild(link);
+//         link.innerHTML = "Section"+[i++];
+//         link.href =section.data-nav;
+//         link.setAttribute('id', 'link');
+//     }
+//     return ul
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
