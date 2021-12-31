618e558324a20d8212e403bda5def8ed7ec3ab76
Ali258-elsharkawy <83187254+Ali258-elsharkawy@users.noreply.github.com>
2021-12-31 05:23:04 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -38,24 +38,6 @@ window.addEventListener('scroll',function(event){
 performance.now();
 
 
-////// I tried tihs method befor asking in stackOverflow and it did not work, can i now how
-///// fix this method? /////////////////
-
-
-
-// function addSection(){
-//     const ul = document.getElementById("navbar__list");
-//     var section = document.querySelectorAll("section");
-//     for( let i= 0; i< section.length; i++){
-//         var li= ul.appendChild(document.createElement("li"));
-//         link=document.createElement("a");
-//         li.appendChild(link);
-//         link.innerHTML = "Section"+[i++];
-//         link.href =section.data-nav;
-//         link.setAttribute('id', 'link');
-//     }
-//     return ul
-// }
 
 
 
